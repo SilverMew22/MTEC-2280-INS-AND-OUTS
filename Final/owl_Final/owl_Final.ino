81c70c2f2a636d61972c35ed9abcb4cf760fb2bc
SilverMew22 <47192158+SilverMew22@users.noreply.github.com>
2020-05-15 10:46:58 -0400

    LED
    Got LED to turn on and off

--- Final/owl_Final/owl_Final.ino
+++ Final/owl_Final/owl_Final.ino
@@ -4,11 +4,10 @@ int photoResistorPin = A0;
 int stepperRot = 200;
 const int LEDa = 5;
 const int LEDb = 4;
-bool light=false;
 Stepper stepper(stepperRot, 6, 9, 10, 11);
 
 void setup()
-{  
+{
   pinMode(LEDa, OUTPUT); // set pin as output
   pinMode(LEDb, OUTPUT); // set pin as output
   stepper.setSpeed(60);
@@ -18,38 +17,29 @@ void setup()
 void loop() {
   int photoPinVal = analogRead(photoResistorPin);
   Serial.println(photoPinVal);
-  if (photoPinVal < 1) {
-    light=true;
-    if(light==true)
-    {digitalWrite(LEDa, HIGH);
-    digitalWrite(LEDb, HIGH);    
-    stepperFun();   
-delay(100);}
-else{light=false;}
-  }  else
-// if   (photoPinVal > 2) 
-{light=false;
-  //{   
-  if(light==false){
+   map(photoPinVal, 0, 20, 0, 255);
+  if (photoPinVal <= 2) {
+    { digitalWrite(LEDa, HIGH);
+      digitalWrite(LEDb, HIGH);  
+      //stepper.step(stepperRot+1);     
+    }
+  } else  {
     digitalWrite(LEDa, LOW);
-     digitalWrite(LEDb, LOW);
-    stepperRot = 0;
-         delay(100);}}
-  //}
+    digitalWrite(LEDb, LOW);    
+  //stepper.step(stepperRot);   
+  }}
 
-}
+
 
 
 
 void stepperFun() {
   for (stepperRot = 0; stepperRot < 180; stepperRot++) {
-    stepper.step(stepperRot); 
-     
+    stepper.step(stepperRot);
     delay(10);
   }
   for (stepperRot = 180; stepperRot > 0; stepperRot--) {
     stepper.step(stepperRot);
-    
     delay(10);
   }
 }
